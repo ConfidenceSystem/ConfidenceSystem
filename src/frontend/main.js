db3e5657a5d85861ed2e3523e68cabb212d3323d
Jack <jack@Jacks-MacBook-Air.local>
2022-01-08 08:13:59 +0000

    frontend forms

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -7,7 +7,53 @@ import './main.sass'
 const Main = () => (
   <div id='Main'>
     <h1>Ethereum-Audit</h1>
-  </div>
+  
+
+<h3>Request Audit</h3>
+<br></br>
+<form onSubmit={(event) => {
+  
+
+}}>
+   <input
+      id="Request Audit"
+      type="text"
+      className="form-control"
+      placeholder="IPFS link to system details and files"
+      required />
+<input type="submit"  />
+</form>
+<br></br>
+
+<h3>Bounty</h3>
+<form onSubmit={(event) => {
+  
+
+}}>
+   <input
+      id="Bounty"
+      type="text"
+      className="form-control"
+      placeholder="bounty"
+      required />
+<input type="submit"  />
+</form>
+<br></br>
+
+<h3>Staking Window</h3>
+<form onSubmit={(event) => {
+  
+
+}}>
+   <input
+      id="Staking Window"
+      type="text"
+      className="form-control"
+      placeholder="staking window"
+      required />
+<input type="submit"  />
+</form>
+</div>
 )
 
 ReactDOM.render(<Main/>, document.getElementById('root'))
